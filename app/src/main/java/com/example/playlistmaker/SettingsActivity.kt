333116d704e81b43ef7eb7643fcfd381e0aef7e7
elkabelaya <elkabelaya@oup.com>
2025-07-30 16:14:37 +0300

    sprint 9

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,15 +1,44 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.MaterialToolbar
+import android.widget.Button
+
 
 class SettingsActivity : AppCompatActivityWithToolBar() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         setupToolBar(getResources().getString(R.string.main_settings))
+        setupListeners()
+    }
+
+    fun setupListeners() {
+        val shareButton = findViewById<Button>(R.id.share)
+        val supportButton = findViewById<Button>(R.id.support)
+        val agreementButton = findViewById<Button>(R.id.agreement)
+
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.setType("text/html")
+            intent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.settings_share_url))
+            startActivity(intent)
+        }
+
+        supportButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, getResources().getString(R.string.settings_support_email))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.settings_support_subject))
+            intent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.settings_support_body))
+            startActivity(intent)
+        }
+
+        agreementButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(getResources().getString(R.string.settings_agreement_url)))
+            startActivity(intent)
+        }
     }
 }
