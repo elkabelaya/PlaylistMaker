512d27e047e9bfbb0ee7e28d52d083b11839aff7
elkabelaya <elkabelaya@oup.com>
2025-07-30 17:08:48 +0300

    Revert "sprint 9"
    This reverts commit 333116d704e81b43ef7eb7643fcfd381e0aef7e7.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,65 +1,15 @@
 package com.example.playlistmaker
-import android.content.pm.ActivityInfo
-import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
 
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivityWithToolBar() {
-    var searchText: String = TEXT_DEF
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         setupToolBar(getResources().getString(R.string.main_search))
-        setupSearchBar()
-
-        // TODO
-        // строка добавлена для демонстрации работы onRestoreInstanceState
-        // при повороте экрана
-        // убрать после ревью
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(TEXT_KEY, searchText)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-        searchText = savedInstanceState.getString(TEXT_KEY, TEXT_DEF)
-        val editText = findViewById<EditText>(R.id.search_text)
-        editText.setText(searchText)
-    }
-
-    private fun setupSearchBar() {
-        val editText = findViewById<EditText>(R.id.search_text)
-        val resetButton = findViewById<ImageView>(R.id.reset_button)
-
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                val currentText = s.toString()
-                resetButton.visibility = if (currentText.count() > 0) View.VISIBLE else View.INVISIBLE
-                searchText = currentText
-            }
-
-            override fun afterTextChanged(s: Editable) {}
-        })
-
-        resetButton.setOnClickListener{
-            editText.setText("")
-            hideKeyboardFrom(this, editText)
-        }
-    }
-
-    companion object {
-        const val TEXT_KEY = "TEXT_KEY"
-        const val TEXT_DEF = ""
     }
 }
