4ea7875908dbff923873fefb2a7f775511e039f5
elkabelaya <elkabelaya@oup.com>
2025-08-04 09:34:52 +0300

    sprint 9

---
+++ app/src/main/java/com/example/playlistmaker/Utils.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity.INPUT_METHOD_SERVICE
+
+fun hideKeyboardFrom(context: Context, view: View) {
+    val imm = context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
